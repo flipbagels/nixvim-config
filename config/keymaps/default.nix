c5f7c288718e0d9f6d1d17414414fddc06fecf41
Lukas Seier <lukas.seier12@gmail.com>
2024-09-20 22:47:55 +0100

    Added some keymaps

---
+++ config/keymaps/default.nix
@@ -0,0 +1,50 @@
+{
+  globals = {
+    mapleader = " ";
+    maplocalleader = " ";
+  };
+
+  keymaps = [
+    { # Clear highlights on search when pressing <Esc> in normal mode
+      mode = "n";
+      key = "<Esc>";
+      action = "<cmd>nohlsearch<CR>";
+    }
+    { # Diagnostic keymaps
+      mode = "n";
+      key = "<leader>q";
+      action = "vim.diagnostic.setloclist";
+      options = { desc = "Open diagnostic [Q]uickfix list"; };
+    }
+    { # Exit terminal with <Esc><Esc> instead of <C-\><C-n>
+      mode = "t";
+      key = "<Esc><Esc>";
+      action = "<C-\\><C-n>";
+      options = { desc = "Exit terminal mode"; };
+    }
+    { # Window switching
+      mode = "n";
+      key = "<C-h>";
+      action = "<C-w><C-h>";
+      options = { desc = "Move focus to the left window"; };
+    }
+    { # Window switching
+      mode = "n";
+      key = "<C-l>";
+      action = "<C-w><C-l>";
+      options = { desc = "Move focus to the right window"; };
+    }
+    { # Window switching
+      mode = "n";
+      key = "<C-j>";
+      action = "<C-w><C-j>";
+      options = { desc = "Move focus to the lower window"; };
+    }
+    { # Window switching
+      mode = "n";
+      key = "<C-k>";
+      action = "<C-w><C-k>";
+      options = { desc = "Move focus to the upper window"; };
+    }
+  ];
+}
