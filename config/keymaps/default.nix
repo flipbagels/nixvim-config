a5af08a24e37bc3b010c1c7472002803da91be48
Lukas Seier <lukas.seier12@gmail.com>
2024-09-23 21:06:10 +0100

    Added jj keymap

--- config/keymaps/default.nix
+++ config/keymaps/default.nix
@@ -5,40 +5,52 @@
   };
 
   keymaps = [
-    { # Clear highlights on search when pressing <Esc> in normal mode
+    {
+      mode = "i";
+      key = "jj";
+      action = "<Esc>";
+    }
+    {
+      # Clear highlights on search when pressing <Esc> in normal mode
       mode = "n";
       key = "<Esc>";
       action = "<cmd>nohlsearch<CR>";
-    } 
-    { # Exit terminal with <Esc><Esc> instead of <C-\><C-n>
+    }
+    {
+      # Exit terminal with <Esc><Esc> instead of <C-\><C-n>
       mode = "t";
       key = "<Esc><Esc>";
       action = "<C-\\><C-n>";
-      options = { desc = "Exit terminal mode"; };
+      options = {desc = "Exit terminal mode";};
     }
-    { # Window switching
+    {
+      # Window switching
       mode = "n";
       key = "<C-h>";
       action = "<C-w><C-h>";
-      options = { desc = "Move focus to the left window"; };
+      options = {desc = "Move focus to the left window";};
     }
-    { # Window switching
+    {
+      # Window switching
       mode = "n";
       key = "<C-l>";
       action = "<C-w><C-l>";
-      options = { desc = "Move focus to the right window"; };
+      options = {desc = "Move focus to the right window";};
     }
-    { # Window switching
+    {
+      # Window switching
       mode = "n";
       key = "<C-j>";
       action = "<C-w><C-j>";
-      options = { desc = "Move focus to the lower window"; };
+      options = {desc = "Move focus to the lower window";};
     }
-    { # Window switching
+    {
+      # Window switching
       mode = "n";
       key = "<C-k>";
       action = "<C-w><C-k>";
-      options = { desc = "Move focus to the upper window"; };
+      options = {desc = "Move focus to the upper window";};
     }
   ];
 }
+
