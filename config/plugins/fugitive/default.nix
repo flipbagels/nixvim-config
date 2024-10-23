ff006fd9d3bfbab1176dd5d50f229da5abb5ec2c
Lukas Seier <lukas.seier12@gmail.com>
2024-10-24 00:11:07 +0100

    Removed keymaps

--- config/plugins/fugitive/default.nix
+++ config/plugins/fugitive/default.nix
@@ -6,33 +6,8 @@
   keymaps = [
     {
       mode = "n";
-      key = "<leader>gg";
-      action = "<cmd>Git<CR>";
-    }
-    {
-      mode = "n";
       key = "<leader>gs";
-      action = "<cmd>Git status<CR>";
-    }
-    {
-      mode = "n";
-      key = "<leader>ga*";
-      action = "<cmd>Git add *<CR>";
-    }
-    {
-      mode = "n";
-      key = "<leader>gp";
-      action = "<cmd>Git push<CR>";
-    }
-    {
-      mode = "n";
-      key = "<leader>gd";
-      action = "<cmd>Git diff<CR>";
-    }
-    {
-      mode = "n";
-      key = "<leader>gc";
-      action = "<cmd>Git commit<CR>";
+      action = "<cmd>Git<CR>";
     }
   ];
 }
