4dc04943c69df1fc39b82a5188c7fb1ab91df3f2
Lukas Seier <lukas.seier12@gmail.com>
2024-10-23 21:57:11 +0100

    Added more keybinds

--- config/plugins/fugitive/default.nix
+++ config/plugins/fugitive/default.nix
@@ -6,8 +6,23 @@
   keymaps = [
     {
       mode = "n";
-      key = "<leader>gs";
+      key = "<leader>g";
       action = "<cmd>Git<CR>";
+    }
+    {
+      mode = "n";
+      key = "<leader>gs";
+      action = "<cmd>Git status<CR>";
+    }
+    {
+      mode = "n";
+      key = "<leader>ga";
+      action = "<cmd>Git add *<CR>";
+    }
+    {
+      mode = "n";
+      key = "<leader>gp";
+      action = "<cmd>Git push<CR>";
     }
   ];
 }
