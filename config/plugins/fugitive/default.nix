6c05ed4f98370b8332a941a906e97dae9060c050
Lukas Seier <lukas.seier12@gmail.com>
2024-10-23 21:37:22 +0100

    Added keymap to enter fugitive

--- config/plugins/fugitive/default.nix
+++ config/plugins/fugitive/default.nix
@@ -2,4 +2,13 @@
   plugins.fugitive = {
     enable = true;
   };
+
+  keymaps = [
+    {
+      # Clear highlights on search when pressing <Esc> in normal mode
+      mode = "n";
+      key = "<leader>gs";
+      action = "<cmd>Git<CR>";
+    }
+  ];
 }
