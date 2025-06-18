58657f7ee9a6ff02b0642356f3daeb6a25c778a8
Lukas Seier <lukas.seier12@gmail.com>
2025-06-18 12:42:11 +0100

    tidal

--- config/plugins/tidal/default.nix
+++ config/plugins/tidal/default.nix
@@ -1,6 +1,10 @@
 { pkgs, ... }:
 
 {
+  # extraPlugins = [
+  #   pkgs.vimPlugins."vim-tidal"
+  # ];
+
   extraPlugins = [
     (pkgs.vimUtils.buildVimPlugin {
       name = "vim-tidal";
@@ -8,8 +12,12 @@
         owner = "tidalcycles";
         repo = "vim-tidal";
         rev = "1.4.8";
-        hash = "sha256-c12v9+s/JspQ9Am291RFI7eg0UAeXGDvJ5lK+7ukOb0="; # replace with actual sha256 hash
+        hash = "sha256-c12v9+s/JspQ9Am291RFI7eg0UAeXGDvJ5lK+7ukOb0=";
       };
     })
   ];
+  extraConfigLua = ''
+    vim.g.tidal_target = "terminal"
+    vim.g.tidal_boot = "ghci -XOverloadedStrings -XExtendedDefaultRules -XDataKinds -XTypeFamilies -XFlexibleContexts -XScopedTypeVariables -package tidal"
+  '';
 }
